4a673bb91ecbe06b3ae1cee0d4ba74196f9394be
Chris Hodapp <hodapp87@gmail.com>
2016-12-19 23:29:41 -0500

    Added separate fetch/search commands

--- Main.hs
+++ Main.hs
@@ -12,44 +12,85 @@ import Data.Text
 
 import qualified Hypothesis
 
-data Config = Config
+-- | Overall options
+data Options = Options
   { host :: String -- ^ hypothes.is server hostname
   , port :: Int -- ^ hypothes.is server port
   , key :: Text -- ^ Developer API key
+  , cmd :: Command
   }
 
-config :: Parser Config
-config = Config <$> strOption
-         (long "host"
-           <> value "hypothes.is"
-           <> metavar "HOSTNAME"
-           <> help "hypothes.is server hostname")
-         <*> option auto
-         (long "port"
-          <> value 443
-          <> metavar "PORT"
-          <> help "hypothes.is server port")
-         <*> (pack <$> strOption
-              (long "key"
-               <> value ""
-               <> metavar "KEY"
-               <> help "Developer API key"))
+-- | Operation 
+data Command = Fetch Hypothesis.AnnotationId
+             | Search SearchParams
 
-endpoint :: BaseUrl
-endpoint = BaseUrl Https "hypothes.is" 443 ""
+data SearchParams = SearchParams
+  { user :: Maybe Text
+  , group :: Maybe Text
+  , tag :: Maybe Text
+  }
+
+optionParse :: Parser Options
+optionParse =
+  Options <$> strOption
+  (long "host"
+    <> value "hypothes.is"
+    <> metavar "HOSTNAME"
+    <> help "hypothes.is server hostname")
+  <*> option auto
+  (long "port"
+    <> value 443
+    <> metavar "PORT"
+    <> help "hypothes.is server port")
+  <*> (pack <$> strOption
+        (long "key"
+          <> value ""
+          <> metavar "KEY"
+          <> help "Developer API key"))
+  <*> cmdParse
+
+cmdParse :: Parser Command
+cmdParse = subparser $
+  command "fetch" (info fetchOpts (progDesc "Fetch annotation by ID")) <>
+  command "search" (info searchOpts (progDesc "Search for annotations"))
+
+fetchOpts :: Parser Command
+fetchOpts = Fetch <$>
+  (pack <$> argument str (metavar "ANNOTATION_ID"
+                          <> help "Annotation ID"))
+
+-- | Utility function to parse an optional 'String', returning either
+-- 'Nothing' (by default) or else some 'Text'.
+parseMaybe :: String -> Parser (Maybe Text)
+parseMaybe s = fmap pack <$> option auto (long s <> value Nothing)
+
+searchOpts :: Parser Command
+searchOpts = Search <$> (SearchParams
+                         <$> parseMaybe "user"
+                         <*> parseMaybe "group"
+                         <*> parseMaybe "tag")
+
+opts :: ParserInfo Options
+opts = info (helper <*> optionParse)
+       (fullDesc
+         <> progDesc "Fetch annotations from hypothes.is"
+         <> header "hypothesis - a test for hypothes.is")
 
 main :: IO ()
 main = do
-  let opts = info (helper <*> config)
-        (fullDesc
-          <> progDesc "Fetch annotations from hypothes.is"
-          <> header "hypothesis - a test for hypothes.is")
   cfg <- execParser opts
   manager <- newManager tlsManagerSettings
-  --res <- runExceptT (Hypothesis.annotation (Just key) annotId manager endpoint)
   let baseUrl = BaseUrl Https (host cfg) (port cfg) ""
-  res <- runExceptT $
-         Hypothesis.search (Just $ key cfg) Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing manager endpoint
-  case res of
-    Left err -> putStrLn $ "Error: " ++ show err
-    Right msg -> print msg
+      k = Just $ key cfg
+  case (cmd cfg) of
+    Fetch annId -> do
+      res <- runExceptT (Hypothesis.annotation k annId manager baseUrl)
+      case res of
+        Left err -> putStrLn $ "Error: " ++ show err
+        Right msg -> print msg
+    Search (SearchParams user_ group_ tag_) -> do
+      res <- runExceptT $
+         Hypothesis.search (Just $ key cfg) Nothing Nothing Nothing Nothing Nothing user_ group_ tag_ Nothing manager baseUrl
+      case res of
+        Left err -> putStrLn $ "Error: " ++ show err
+        Right msg -> print msg
