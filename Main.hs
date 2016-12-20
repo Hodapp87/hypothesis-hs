8d7ed5e850f6cc141f4faad2e3fecab6a883df8f
Chris Hodapp <hodapp87@gmail.com>
2016-12-19 22:40:06 -0500

    Added some commandline option parsing

--- Main.hs
+++ Main.hs
@@ -6,25 +6,50 @@ import Control.Monad.Trans.Except (ExceptT, runExceptT)
 import Network.HTTP.Client (Manager, newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Servant.Client
+import Options.Applicative
 
 import Data.Text
 
 import qualified Hypothesis
 
+data Config = Config
+  { host :: String -- ^ hypothes.is server hostname
+  , port :: Int -- ^ hypothes.is server port
+  , key :: Text -- ^ Developer API key
+  }
+
+config :: Parser Config
+config = Config <$> strOption
+         (long "host"
+           <> value "hypothes.is"
+           <> metavar "HOSTNAME"
+           <> help "hypothes.is server hostname")
+         <*> option auto
+         (long "port"
+          <> value 443
+          <> metavar "PORT"
+          <> help "hypothes.is server port")
+         <*> (pack <$> strOption
+              (long "key"
+               <> value ""
+               <> metavar "KEY"
+               <> help "Developer API key"))
+
 endpoint :: BaseUrl
 endpoint = BaseUrl Https "hypothes.is" 443 ""
 
--- See https://hypothes.is/account/developer
-key :: Text
-key = "nope"
-
-annotId :: Text
-annotId = "_BlJDsYvEeav_R9rNP_9VA"
-
 main :: IO ()
 main = do
+  let opts = info (helper <*> config)
+        (fullDesc
+          <> progDesc "Fetch annotations from hypothes.is"
+          <> header "hypothesis - a test for hypothes.is")
+  cfg <- execParser opts
   manager <- newManager tlsManagerSettings
-  res <- runExceptT (Hypothesis.annotation (Just key) annotId manager endpoint)
+  --res <- runExceptT (Hypothesis.annotation (Just key) annotId manager endpoint)
+  let baseUrl = BaseUrl Https (host cfg) (port cfg) ""
+  res <- runExceptT $
+         Hypothesis.search (Just $ key cfg) Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing manager endpoint
   case res of
     Left err -> putStrLn $ "Error: " ++ show err
     Right msg -> print msg
--- README.md
+++ README.md
@@ -3,7 +3,8 @@ hypothesis-hs
 
 This is Haskell bindings
 (via [servant](https://hackage.haskell.org/package/servant)) for the
-web API of [hypothes.is](https://hypothes.is/).  This requires
+web API of [hypothes.is](https://hypothes.is/).  For private
+annotations (possibly other things) this requires
 a
 [developer API token](https://hypothes.is/blog/introducing-developer-api-tokens/).
 
@@ -14,3 +15,6 @@ the [web API](https://h.readthedocs.io/en/latest/api/) are supported:
 - [search](https://h.readthedocs.io/en/latest/api/#operation/search)
 
 This code is mostly scratch work at the moment.
+
+Inspired partly
+by [pypothesis](https://github.com/kshaffer/pypothesis).
